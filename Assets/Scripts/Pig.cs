58bc2e7784b2fd754dcf090fec4d929b1d2d325d
deslrey <2135232170@qq.com>
2025-07-04 11:27:21 +0800

    猪的受伤

---
+++ Assets/Scripts/Pig.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pig : MonoBehaviour
+{
+
+    public float maxSpeed = 8;
+    public float minSpeed = 4;
+    public SpriteRenderer render;
+    public Sprite hurt;
+
+    private void Awake()
+    {
+        render = GetComponent<SpriteRenderer>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.relativeVelocity.magnitude > maxSpeed)    //  Ö±½ÓËÀÍö
+        {
+            Destroy(gameObject);
+        }else if (collision.relativeVelocity.magnitude > minSpeed && collision.relativeVelocity.magnitude < maxSpeed)  //  ÊÜÉË
+        {
+            render.sprite = hurt;
+        }
+    }
+}
